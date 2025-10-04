93fb88af98a8b086f18401c5427645f4e328c879
Mohammad Sadeq Kazemi <mskdev.ir@gmail.com>
2025-10-05 02:50:37 +0330

    create project from scratch

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Home from "./Home.jsx";
+import Products from "./Products.jsx";
+import Stores from "./Stores.jsx";
+import OurStory from "./OurStory.jsx";
+import Blog from "./Blog.jsx";
+import Navbar from "../components/Navbar.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="products" element={<Products />} />
+      <Route path="Stores" element={<Stores />} />
+      <Route path="ourStory" element={<OurStory />} />
+      <Route path="blog" element={<Blog />} />
+    </Routes>
+  </BrowserRouter>
+);
